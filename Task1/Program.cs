0face32a9e6ed888af1d8d494eb79f4948048d8b
Anastasia Grigoreva <strannochel@gmail.com>
2024-06-25 14:39:43 +0200

    Made Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+	
+	void PrintRange(int i, int lim) {
+	     if (i <= lim) 
+	    {
+		Console.Write($"{i}, ");
+		PrintRange(i + 1, lim);
+	}
+	}
+
+    PrintRange(M, N);
