bf8ca92507c6299d5e14434fbf390f2306c615a3
Anastasia Grigoreva <strannochel@gmail.com>
2024-06-25 14:45:06 +0200

    Changed Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Введите значение M: ");
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+Console.Write("Введите значение M: ");
 int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение N: ");
 int N = Convert.ToInt32(Console.ReadLine());
